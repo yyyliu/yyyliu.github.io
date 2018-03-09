1dbf4818ad8467f0d32e7abe3e777b4d8b74483a
Yang Liu <yliu0@cs.washington.edu>
2018-03-09 15:23:56 -0800

    + initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
