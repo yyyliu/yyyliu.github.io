f381c502159495e081b42ffad5be0c64f0dfe1e8
Yang Liu <yliu0@cs.washington.edu>
2019-02-08 11:44:03 -0800

    Interview recruitment page

---
+++ src/pages/RecruitPage.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="bg-theme">
+    <!--Banner-->
+    <div class="banner"></div>
+
+    <!--Content-->
+    <div class="content">
+      <div class="form">
+        <div class="form-border"></div>
+        <div class="form-content">
+
+          <h3 class="mb-4">Statistical Analysis Interview</h3>
+
+          <p>
+            Have you conducted experiments and performed hypothesis testing?
+            Are you willing to share your data and analysis scripts with us?
+            If so, we would like to invite you to participate in a study about understanding the process of statistical analysis.
+
+            <br><br>
+            In the study, we will ask you open-ended questions about the statistical analysis decisions in one of your previous projects.
+            If you have concerns about us inspecting your analysis scripts,
+            please rest assured that our goal is not to find flaws and we will not publish anything without your consent.
+            Instead, we seek to understand analysis processes better.
+
+          </p>
+
+          <div>
+            <div class="mb-2"><b class="font-weight-bold">Requirement:</b> a publication involving statistical analysis</div>
+            <div class="mb-2"><b class="font-weight-bold">Duration:</b> 60 to 90 minutes</div>
+            <div class="mb-2"><b class="font-weight-bold">Method:</b> remote interview</div>
+            <div><b class="font-weight-bold">Reward:</b> USD $20</div>
+          </div>
+          <br>
+
+          <p>
+            If you are interested, please provide your information below.
+            We will contact you soon to set up a time if you are eligible.
+            If you have any questions or concerns about the study,
+            feel free to email Yang Liu (<a href="mailto:yliu0@cs.washington.edu">yliu0@cs.washington.edu</a>).
+          </p>
+
+          <hr>
+          <div>
+
+            <!--Form-->
+            <b-form @submit="onSubmit">
+              <b-form-group label="Email address:"
+                            label-for="emailInput">
+                <b-form-input id="emailInput"
+                              type="email"
+                              v-model="form.email"
+                              required
+                              placeholder="Enter email">
+                </b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Name:"
+                            label-for="nameInput">
+                <b-form-input id="nameInput"
+                              type="text"
+                              v-model="form.name"
+                              required
+                              placeholder="Enter your name">
+                </b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Link to a publication involving statistical analysis:"
+                            label-for="paperInput">
+                <b-form-input id="paperInput"
+                              type="text"
+                              v-model="form.paper"
+                              required
+                              placeholder="Enter a URL to your paper">
+                </b-form-input>
+              </b-form-group>
+            </b-form>
+
+            <!--Submit button-->
+            <button type="submit" class="btn btn-theme mt-3">Submit</button>
+
+            <div class="mt-2" v-if="!showConsent">
+              <small class="text-muted">
+                By proceeding, you agree to the terms.
+                <b-link @click="showConsent=true"> View</b-link>
+              </small>
+            </div>
+
+          </div>
+
+          <!--Disclaimer-->
+          <div v-if="showConsent"
+               class="text-muted mt-3" style="font-size: 0.8em;">
+            By proceeding, you agree to the following terms.
+            You must be over 18 in age to participate.
+            You must be cleared to work in the US to receive payments.
+            Your participation is voluntary.
+            All of the information will be confidential, only accessible by approved research collaborators.
+            We will only use your contact information to contact you if your are eligible for the study.
+            None of the personal information will be included in the study result.
+            If you have any questions or concerns, please contact Yang Liu (yliu0@cs.washington.edu).
+            If you would like to talk to someone separate from the research team about a concern or
+            complaint about your rights as a possible research subject,
+            please contact the University of Washington Institutional Review Board at (206) 543-0098.
+            We cannot ensure the confidentiality of any information sent by email.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Footer-->
+    <div class="footer text-center">
+      <div class="text-muted">
+        <small>This study is conducted by researchers at Interactive Data Lab, University of Washington</small>
+      </div>
+
+      <div class="mt-3">
+        <a href="http://idl.cs.washington.edu" target="_blank">
+          <img class="img-logo" src="/build/images/idl-logo.png"/>
+        </a>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "RecruitPage",
+    data () {
+      return {
+        showConsent: false,
+        form: {
+          email: '',
+          name: '',
+          paper: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit (evt) {
+        evt.preventDefault();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .banner {
+    background-image: url('/build/images/interview-banner.jpeg');
+    background-size: cover;
+    background-position: center;
+    color: rgba(255, 255, 255, 1);
+    height: 278px;
+  }
+
+  .content {
+    margin: auto;
+    width: 770px;
+  }
+
+  .form {
+    margin-top: -100px;
+    background-color: #fff;
+    box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);
+    padding-bottom: 6px;
+    margin-bottom: 32px;
+    word-wrap: break-word;
+  }
+
+  .form-border {
+    background-color: rgb(187, 129, 135);
+    height: 8px;
+  }
+
+  .form-content {
+    color: rgba(0,0,0,.87);
+    padding: 34px;
+    font-size: 16px;
+  }
+
+  .footer {
+    height: 122px;
+  }
+
+  .img-logo {
+    width: 50px;
+  }
+
+  @media (max-width: 800px) {
+    .banner {
+      height: 122px;
+    }
+
+    .content {
+      width: 90%;
+    }
+
+    .form {
+      margin-top: -50px;
+    }
+  }
+
+  .btn-theme {
+    color: #fff;
+    font-weight: 700;
+    background-color: rgb(187, 129, 135);
+    border-color: rgb(187, 129, 135);
+  }
+
+  .btn-theme:hover {
+    background-color: rgb(167, 109, 115);
+    border-color: rgb(167, 109, 115);
+  }
+
+  .btn-theme:focus {
+    box-shadow: 0 0 0 0.2rem rgba(187, 129, 135,.25);
+  }
+
+  .bg-theme {
+    background-color: #f1eced;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,36 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '../pages/MainPage.vue'
 import VitaPage from '../pages/VitaPage.vue'
+import RecruitPage from '../pages/RecruitPage.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
+      meta: { title: 'Yang Liu' },
       component: MainPage
     },
     {
       path: '/cv',
       name: 'cv',
+      meta: { title: 'Yang Liu' },
       component: VitaPage
+    },
+    {
+      path: '/interview',
+      name: 'interview',
+      meta: { title: 'Interview Study' },
+      component: RecruitPage
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+export default router
